43ccb51c2e69e61b50fcda017c4c41a1e5f504d7
Andrey Golubenko <a.g.07.08.78@gmail.com>
2021-05-16 22:21:54 +0300

    improve optimization

--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,26 @@ const isProd = !isDev;
 // function for optimization
 const optimization = () => {
   const config = {
-      splitChunks: {
+      /*splitChunks: {
           chunks: 'all'
+      },
+      runtimeChunk: 'single'*/
+      splitChunks: {
+          cacheGroups: {
+              vendors: {
+                  name: `chunk-vendors`,
+                  test: /[\\/]node_modules[\\/]/,
+                  priority: -10,
+                  chunks: 'initial'
+              },
+              common: {
+                  name: `chunk-common`,
+                  minChunks: 2,
+                  priority: -20,
+                  chunks: 'initial',
+                  reuseExistingChunk: true
+              }
+          }
       },
       runtimeChunk: 'single'
   };
